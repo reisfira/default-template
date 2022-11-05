e5674ac89f067fc27acff23c24dee7595d7522e1
Galuf <mohamadyuzrie@gmail.com>
2022-11-05 23:18:44 +0800

    set default height if hide white header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.master', ['hide_white_header' => true])
 @section('title', 'Home')
 
 @section('content')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
-@if(!isset($noWhiteHeader))
+@if(!isset($hide_white_header))
     <div class="bg-white pt-0 pb-0 {{ isset($no_padding_bottom) ? '' : 'mb-3' }}">
         <div class="row" style="width:99.9%">
             <div class="col-1 pr-0">
-                @if(!isset($noBackButton))
+                @if(!isset($hide_back_button))
                 <ol class="breadcrumb bg-white pr-0 m-0">
                     <li class="breadcrumb-item pr-0 m-0">
                         <a href="#" onclick="history.go(-1)"><i class="fa fa-angle-left pr-1" aria-hidden="true"></i>
@@ -22,4 +22,6 @@
             </div>
         </div>
     </div>
+@else
+<div class="mb-3"></div>
 @endif
